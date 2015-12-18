3d9848723781d714fa08c3f7c00c18744014fa07
Alexandr Shestak <alexandr@Alexandr-iMac.local>
2015-12-18 11:46:29 +0300

    add urls check function

--- src/main/java/T27058.java
+++ src/main/java/T27058.java
@@ -4,31 +4,57 @@
 
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Optional;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class T27058 {
 
-    private static String[] urls = new String[]{
+    static int currentAttempt = 0;
 
+    private static String[] urls = new String[]{
+            "https://www.sgscc.edu.au/courses/SchoolAge?debugQuery=true",
+            "https://www.sgscc.edu.au/courses/Leisure/Fashion+Sewing?debugQuery=true",
+            "https://www.sgscc.edu.au/courses?s=&tag=/SchoolAge&near=&km=&price=&time=&day=&debugQuery=true"
+    };
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
+        while (true) {
+            for (String url : urls) {
+                if (isFailed(url)) {
+                    return;
+                }
+                Thread.sleep(1000);
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        boolean found = false;
-        while (!found) {
-            open("https://www.sgscc.edu.au/courses/SchoolAge?debugQuery=true");
-            SelenideElement courseList = $("#courses-list");
-            ElementsCollection h2s = $$(courseList, "h2");
-            SelenideElement noResults = h2s.stream().filter(p -> p.innerText().equals("No results")).findFirst().get();
-            System.out.println(noResults);
+    private static boolean isFailed(String url) throws FileNotFoundException, UnsupportedEncodingException {
+        currentAttempt++;
+        open(url);
+        SelenideElement courseList = $("#courses-list");
+        ElementsCollection h2s = $$(courseList, "h2");
 
+        Optional<SelenideElement> noResults = h2s
+                .stream()
+                .filter(p -> p.innerText().equals("No results"))
+                .findFirst();
 
-//            System.out.println(courses);
-//            if (courses.size() < 1) {
-//                Selenide.screenshot("NoResult");
-//                found = true;
-//            }
+        if (noResults.isPresent()) {
+            Selenide.screenshot("NoResult");
+            PrintWriter report = new PrintWriter("report.txt", "UTF-8");
+            report.println("Attempt count : " + currentAttempt);
+            report.println("Failed url : " + url);
+            report.close();
+            return true;
         }
+        return false;
     }
 }
